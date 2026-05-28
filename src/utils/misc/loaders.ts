b488d245775ebeb060bbcbace8201c7b88dc197c
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-05-28 17:21:23 +0300

    feat: add aura-route - base element for routing processing

---
+++ src/utils/misc/loaders.ts
@@ -0,0 +1,24 @@
+import { registerComponent } from './component'
+
+/** For path correct from subfolder (utils/misc) to the root folder (src) */
+const pathPrefix = '../../'
+
+export const loadComponent = (path: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    import(pathPrefix + path).then((exports) => {
+      Object.values(exports).forEach((Component: any) => {
+        const tagName = Component?.is
+        if (typeof tagName === 'string') {
+          registerComponent(Component)
+          resolve(`<${tagName}></${tagName}>`)
+        } else {
+          reject('Invalid tagName')
+          console.log(`[AURA UI] did not found [IS] property for the component: ${path}`)
+        }
+      })
+      return exports
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}

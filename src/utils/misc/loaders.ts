a6255158f2f55e80fa7ced318cc3287bc3f327d5
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-05-29 20:57:41 +0300

    chore: code refactoring, added getContent, new loaders functions

--- src/utils/misc/loaders.ts
+++ src/utils/misc/loaders.ts
@@ -1,9 +1,26 @@
 import { registerComponent } from './component'
 
+const sleep = (time: number) => new Promise(resolve => setTimeout(resolve, time))
+
+export const sanitizeHtml = (html: string) => {
+  const $template = document.createElement('template')
+  $template.innerHTML = html
+  return $template.content.cloneNode(true)
+}
+
+export const loadContent = async (url: string, signal: AbortSignal): Promise<string> => {
+  const response = await fetch(url, { signal })
+  if (!response.ok) return ''
+
+  await sleep(1000)
+
+  return await response.text()
+}
+
 /** For path correct from subfolder (utils/misc) to the root folder (src) */
 const pathPrefix = '../../'
 
-export const loadComponent = (path: string): Promise<any> => {
+export const loadAndRegisterComponent = (path: string): Promise<any> => {
   return new Promise((resolve, reject) => {
     import(pathPrefix + path).then((exports) => {
       Object.values(exports).forEach((Component: any) => {

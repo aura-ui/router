6a1a1ced8f85f45ae4050cb1d897f6bcd9e90c7a
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-06-20 16:29:42 +0300

    chore: cleanup old code

--- src/modules/aura-routing-engine/core.ts
+++ src/modules/aura-routing-engine/core.ts
@@ -17,21 +17,3 @@ export type {
 } from './core/types';
 
 export { PHASE_PIPELINE } from './core/types';
-
-export type { NavigoRoutingStrategy, NavigoProviderConfig } from './core/providers/navigo-config';
-
-export { NavigoProvider, createNavigoProvider } from './core/providers/navigo-provider';
-
-export type { RoutingEngineOptions, NavigationHandler } from './core/routing-engine';
-
-export type {
-  RoutingEngineProvider,
-  RoutingProviderFactory,
-} from './core/provider';
-
-export { RoutingProviderRegistry } from './core/provider-registry';
-export { RoutingEngine } from './core/routing-engine';
-
-import { registerBuiltInProviders } from './core/providers';
-
-registerBuiltInProviders();

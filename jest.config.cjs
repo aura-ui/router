2df5ab2c1289826d91b5595ad0ecca0abe3fe7cb
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-06-24 18:04:47 +0300

    feat: add invalidate and SWR mode to cache

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     '<rootDir>/src/modules/aura-dom/test',
     '<rootDir>/src/modules/aura-outlet/test',
     '<rootDir>/src/modules/aura-route/test',
+    '<rootDir>/src/modules/aura-utils/test',
   ],
   testMatch: ['**/*.test.ts'],
   transform: {

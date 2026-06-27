f37f73c4034d98c5b6f901dee417dbdaa86b229d
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-06-27 17:06:16 +0300

    refactor: removed route v1

--- src/modules/aura-route/core.ts
+++
@@ -1,9 +0,0 @@
-export * from './core/aura-route';
-export {
-  RouteViewCache,
-  defaultRouteViewCache,
-  type RouteViewCachePort,
-} from './core/view/view-cache';
-export {
-  viewCacheKey,
-} from './core/view/view-cache-key';

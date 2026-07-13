ce848737177e415249e271cb4c6b39680a7e82dc
yadroRus <37119397+yadroRus@users.noreply.github.com>
2026-07-13 03:05:23 +0300

    refactor: cleanup old code

--- src/modules/aura-routing-engine/test/content/cache-barrel.test.ts
+++
@@ -1,8 +0,0 @@
-import { NO_CACHE, parseCacheAttr } from '../../core/content';
-
-describe('parseCacheAttr (content barrel)', () => {
-  it('re-exports cache parser from aura-route', () => {
-    expect(parseCacheAttr('data')).toEqual({ dom: false, view: false, data: true });
-    expect(parseCacheAttr(null)).toEqual(NO_CACHE);
-  });
-});
